a661042e05bd677c3ff532180b6e08c5fdd12842
Wellington Viveiro <wviveiro@atlassian.com>
2024-05-28 16:55:17 +1000

    Error when clicking a button with enter

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,17 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("page", () => {
+  it("works", () => {
+    cy.visit("./index.html");
+    cy.get("#clicked-01").should("have.text", "0");
+    cy.get("#submitted-01").should("have.text", "0");
+    cy.get("#btn-click").click();
+    cy.get("#clicked-01").should("have.text", "1");
+    cy.get("#submitted-01").should("have.text", "0");
+    cy.get("#submit-click").click();
+    cy.get("#clicked-01").should("have.text", "1");
+    cy.get("#submitted-01").should("have.text", "1");
+    cy.get("#btn-click").focus().type("{enter}");
+    cy.get("#clicked-01").should("have.text", "2");
+    cy.get("#submitted-01").should("have.text", "1"); // ENTER ON BUTTON SHOULD NOT SUBMIT FORM
+  });
+});
